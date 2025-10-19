d64b0f1f73117a77b9032b6e78bb302c369c4f54
TomThot <tom.thot@gmail.com>
2025-10-19 22:13:22 +0200

    test menu clic

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 document.addEventListener('DOMContentLoaded', function () {
   const burger = document.getElementById('burger-toggle');
   const bandeau = document.getElementById('bandeau');
-  const test=0;
+
   burger.addEventListener('click', () => {
     bandeau.classList.toggle('open');
-    burger.textContent = bandeau.classList.contains('open') ? '✖' : '☰';
+    burger.textContent = bandeau.classList.contains('open') ? '✖ Menu' : '☰ Menu';
   });
+
+  // Toggle des sous-menus uniquement en responsive
+  if (window.innerWidth <= 768) {
+    const toggleMenus = document.querySelectorAll('.menu.toggleable .titre');
+    toggleMenus.forEach(titre => {
+      titre.addEventListener('click', () => {
+        const parentMenu = titre.closest('.menu');
+        parentMenu.classList.toggle('active');
+      });
+    });
+  }
 });
