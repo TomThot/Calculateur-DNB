1c830db281598180840c3eca0078e780be2e1846
TomThot <tom.thot@gmail.com>
2025-10-19 18:28:10 +0200

    responsive burger

--- Script.js
+++ Script.js
@@ -1,6 +1,9 @@
-const burger = document.getElementById('burger-toggle');
-      const bandeau = document.getElementById('bandeau');
+document.addEventListener('DOMContentLoaded', function () {
+  const burger = document.getElementById('burger-toggle');
+  const bandeau = document.getElementById('bandeau');
 
-      burger.addEventListener('click', () => {
-        bandeau.classList.toggle('open');
-      });
+  burger.addEventListener('click', () => {
+    bandeau.classList.toggle('open');
+    burger.textContent = bandeau.classList.contains('open') ? '✖' : '☰';
+  });
+});
