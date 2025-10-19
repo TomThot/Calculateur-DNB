ff1897b6d240b190d0921de2834bbf7be2df8340
TomThot <tom.thot@gmail.com>
2025-10-19 18:47:08 +0200

    v2.3 responsive

--- Script.js
+++ Script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
   const burger = document.getElementById('burger-toggle');
   const bandeau = document.getElementById('bandeau');
-
+  const test=0;
   burger.addEventListener('click', () => {
     bandeau.classList.toggle('open');
     burger.textContent = bandeau.classList.contains('open') ? '✖' : '☰';
